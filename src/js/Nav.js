ec7b000049e8a99125ff359badb960be7c49d1c5
Bartosz Mediger <bartosz.mediger@withpassion.pl>
2017-06-16 01:07:26 +0200

    RWD

--- src/js/Nav.js
+++ src/js/Nav.js
@@ -8,18 +8,56 @@
 		var trigger = $('.js-nav'),
 			menu = $('.js-menu'),
 			top = $('.c-top'),
-			body = document.getElementsByTagName('body');
+			body = document.getElementsByTagName('body'),
+			detached = false,
+			listenKeys,
+			m;
+		
+		function hideMenu() {
+			m = menu.detach();
+			top.append(m);
+			
+			$(body).removeClass('no-scroll mobile-nav');
+			document.removeEventListener('keydown', listenKeys);
+		}
+		
+		function showMenu() {
+			top.after(m);
+			
+			listenKeys = function(evt) {			
+			    evt = evt || window.event;
+			    if (evt.keyCode == 27) {
+			    	hideMenu(m);
+			    }
+			};
+
+			document.addEventListener('keydown', listenKeys, false);
+		
+			if (body[0].classList) {
+			    body[0].classList.add('no-scroll');
+			} else {
+			    body[0].className += ' no-scroll';
+			}
+		}
 		
 		trigger.on('click', function(e) {
 			e.preventDefault();
 
-			$(body).toggleClass('mobile-nav-active');
+			m = menu.detach();
 
-			var m = menu.detach();
+			$(body).toggleClass('mobile-nav');
 			
-			top.after(m);
+			if (detached === false) {
+				showMenu();
+				detached = true;
+			
+			} else {
+				hideMenu();
+				detached = false;
+			}
 		});
 	};
+
 
 	tesco.Nav = new Nav();
 
