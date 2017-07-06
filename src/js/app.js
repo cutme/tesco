c38a1554015ce6b71dd7fa3bd0c56264d4b749b7
Bartosz Mediger <bartosz.mediger@withpassion.pl>
2017-07-06 11:08:40 +0200

    Top height on list

--- src/js/app.js
+++ src/js/app.js
@@ -18,7 +18,7 @@
 		if (tesco.Helper.exist('.js-openGallery')) new tesco.Gallery();
 		if (tesco.Helper.exist('.js-upload')) new tesco.Upload.init();
 		if (tesco.Helper.exist('.nice-select')) tesco.Helper.nSelect();		
-//		if (tesco.Helper.exist('[data-toggle="datepicker"]')) tesco.Datepicker.init();
+		if (tesco.Helper.exist('[data-toggle="datepicker"]')) tesco.Datepicker.init();
 		
 		if (tesco.Helper.exist('#autocomplete')) tesco.Autocomplete.init();
 		if (tesco.Helper.exist('#list')) tesco.Vote.init();
